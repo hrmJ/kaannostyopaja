88f760f44d6009623e6fdc56a3b5bbe2ab0235ce
Juho Härme <juho.harme@uta.fi>
2015-09-16 17:34:47 +0300

    start making the guide

---
+++ mdtohtmlwithjs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+filename=$(basename "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+DIR=$(dirname "$1")
+#Convert the knitted md to the desired output format
+pandoc --toc -V lang=finnish \
+    --number-sections \
+    --include-in-header=scripts.js \
+    --include-in-header=docstyles.css \
+    -s $DIR/$filename.$extension -o $DIR/$filename.html
+
+
