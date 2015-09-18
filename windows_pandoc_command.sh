9802a68339f0bdb5b519e13e7c81dca02f9a4de3
Juho Härme <juho.harme@uta.fi>
2015-09-18 10:03:03 +0300

    version1.1

--- windows_pandoc_command.sh
+++ windows_pandoc_command.sh
@@ -1,1 +1,1 @@
-!pandoc --toc -V lang=finnish --number-sections --include-in-header=scripts.js --include-in-header=docstyles.css --include-before-body=header.html -s trados_opas.Rmd -o trados_opas.html
+pandoc --toc -V lang=finnish --number-sections --include-in-header=scripts.js --include-in-header=docstyles.css --include-before-body=header.html -s trados_opas.Rmd -o trados_opas.html
