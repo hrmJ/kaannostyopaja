cb6b9f9578dfc5d5838b211147125124d1b78ab4
Juho Härme <juho.harme@uta.fi>
2017-08-23 13:35:53 +0300

    lsiää vaihtoehto esimerkkimetodille

--- windows_pandoc_command.sh
+++ windows_pandoc_command.sh
@@ -1,2 +1,3 @@
 #pandoc --toc -V lang=finnish --number-sections --include-in-header=scripts.js --include-in-header=docstyles.css --include-before-body=header.html -s trados_opas.Rmd -o trados_opas.html
 pandoc --toc -V lang=finnish  --include-in-header=scripts.js --include-in-header=docstyles.css --include-before-body=header.html -s trados_opas.Rmd -o trados_opas.html
+python3 indexer.py
